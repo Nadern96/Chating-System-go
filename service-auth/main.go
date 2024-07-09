c3734657e162621ae95e48bf9cab8851ff0dd4af
Nadern96 <nadernabilmansour@gmail.com>
2024-07-10 00:50:44 +0300

    created service-auth

--- service-auth/main.go
+++ service-auth/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
 func main() {
 
+}
+
+func ListenGRPC(port string, registerFn func(*grpc.Server), opt ...grpc.ServerOption) {
+	grpcServer := grpc.NewServer(opt...)
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	registerFn(grpcServer)
+	go func() {
+		err := grpcServer.Serve(lis)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	return grpcServer
 }
